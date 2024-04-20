19e27dbe7c8e786bdb5bf4b50c6f8889e0908cab
yashhhhhhhhh504 <yash21504@iiitd.ac.in>
2024-04-20 20:36:39 +0530

    first commit

---
+++ Q2_mc/LandmarkRecognitionTensorflow-final/app/src/main/java/com/plcoding/landmarkrecognitiontensorflow/MainActivity.kt
@@ -0,0 +1,225 @@
+package com.plcoding.landmarkrecognitiontensorflow
+import android.graphics.Bitmap
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import androidx.camera.view.CameraController
+import androidx.camera.view.LifecycleCameraController
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.plcoding.landmarkrecognitiontensorflow.ui.theme.LandmarkRecognitionTensorflowTheme
+import org.tensorflow.lite.support.image.ImageProcessor
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.task.core.BaseOptions
+import org.tensorflow.lite.task.core.vision.ImageProcessingOptions
+import org.tensorflow.lite.task.vision.classifier.ImageClassifier
+data class Classification(
+    val name: String,
+    val score: Float
+)
+interface LandmarkClassifier {
+    fun classify(bitmap: Bitmap, rotation: Int): List<Classification>
+}
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if(!hasCameraPermission()) {
+            ActivityCompat.requestPermissions(
+                this, arrayOf(Manifest.permission.CAMERA), 0
+            )
+        }
+        setContent {
+            LandmarkRecognitionTensorflowTheme {
+                var classifications by remember {
+                    mutableStateOf(emptyList<Classification>())
+                }
+                val analyzer = remember {
+                    LandmarkImageAnalyzer(
+                        classifier = TfLiteLandmarkClassifier(
+                            context = applicationContext
+                        ),
+                        onResults = {
+                            classifications = it
+                        }
+                    )
+                }
+                val controller = remember {
+                    LifecycleCameraController(applicationContext).apply {
+                        setEnabledUseCases(CameraController.IMAGE_ANALYSIS)
+                        setImageAnalysisAnalyzer(
+                            ContextCompat.getMainExecutor(applicationContext),
+                            analyzer
+                        )
+                    }
+                }
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                ) {
+                    CameraPreview(controller, Modifier.fillMaxSize())
+
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .align(Alignment.TopCenter)
+                    ) {
+                        classifications.forEach {
+                            Text(
+                                text = it.name,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .background(MaterialTheme.colorScheme.primaryContainer)
+                                    .padding(8.dp),
+                                textAlign = TextAlign.Center,
+                                fontSize = 20.sp,
+                                color = MaterialTheme.colorScheme.primary
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun hasCameraPermission() = ContextCompat.checkSelfPermission(
+        this, Manifest.permission.CAMERA
+    ) == PackageManager.PERMISSION_GRANTED
+}
+
+class TfLiteLandmarkClassifier(
+    private val context: Context,
+    private val threshold: Float = 0.5f,
+    private val maxResults: Int = 3
+): LandmarkClassifier {
+
+    private var classifier: ImageClassifier? = null
+
+    private fun setupClassifier() {
+        val baseOptions = BaseOptions.builder()
+            .setNumThreads(2)
+            .build()
+        val options = ImageClassifier.ImageClassifierOptions.builder()
+            .setBaseOptions(baseOptions)
+            .setMaxResults(maxResults)
+            .setScoreThreshold(threshold)
+            .build()
+
+        try {
+            classifier = ImageClassifier.createFromFileAndOptions(
+                context,
+                "landmarks.tflite",
+                options
+            )
+        } catch (e: IllegalStateException) {
+            e.printStackTrace()
+        }
+    }
+
+    override fun classify(bitmap: Bitmap, rotation: Int): List<Classification> {
+        if(classifier == null) {
+            setupClassifier()
+        }
+
+        val imageProcessor = ImageProcessor.Builder().build()
+        val tensorImage = imageProcessor.process(TensorImage.fromBitmap(bitmap))
+
+        val imageProcessingOptions = ImageProcessingOptions.builder()
+            .setOrientation(getOrientationFromRotation(rotation))
+            .build()
+
+        val results = classifier?.classify(tensorImage, imageProcessingOptions)
+
+        return results?.flatMap { classications ->
+            classications.categories.map { category ->
+                Classification(
+                    name = category.displayName,
+                    score = category.score
+                )
+            }
+        }?.distinctBy { it.name } ?: emptyList()
+    }
+
+    private fun getOrientationFromRotation(rotation: Int): ImageProcessingOptions.Orientation {
+        return when(rotation) {
+            android.view.Surface.ROTATION_270 -> ImageProcessingOptions.Orientation.BOTTOM_RIGHT
+            android.view.Surface.ROTATION_90 -> ImageProcessingOptions.Orientation.TOP_LEFT
+            android.view.Surface.ROTATION_180 -> ImageProcessingOptions.Orientation.RIGHT_BOTTOM
+            else -> ImageProcessingOptions.Orientation.RIGHT_TOP
+        }
+    }
+}
+
+class LandmarkImageAnalyzer(
+    private val classifier: LandmarkClassifier,
+    private val onResults: (List<Classification>) -> Unit
+): ImageAnalysis.Analyzer {
+
+    private var frameSkipCounter = 0
+
+    override fun analyze(image: ImageProxy) {
+        if(frameSkipCounter % 60 == 0) {
+            val rotationDegrees = image.imageInfo.rotationDegrees
+            val bitmap = image
+                .toBitmap()
+                .centerCrop(321, 321)
+
+            val results = classifier.classify(bitmap, rotationDegrees)
+            onResults(results)
+        }
+        frameSkipCounter++
+
+        image.close()
+    }
+}
+@Composable
+fun CameraPreview(
+    controller: LifecycleCameraController,
+    modifier: Modifier = Modifier
+) {
+    val lifecycleOwner = LocalLifecycleOwner.current
+    AndroidView(
+        factory = {
+            PreviewView(it).apply {
+                this.controller = controller
+                controller.bindToLifecycle(lifecycleOwner)
+            }
+        },
+        modifier = modifier
+    )
+}
+fun Bitmap.centerCrop(desiredWidth: Int, desiredHeight: Int): Bitmap {
+    val xStart = (width - desiredWidth) / 2
+    val yStart = (height - desiredHeight) / 2
+
+    if(xStart < 0 || yStart < 0 || desiredWidth > width || desiredHeight > height) {
+        throw IllegalArgumentException("Invalid arguments for center cropping")
+    }
+
+    return Bitmap.createBitmap(this, xStart, yStart, desiredWidth, desiredHeight)
+}
